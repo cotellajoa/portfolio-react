b474c9d863a2ac652970dcb531d53f1587220f01
cotellajoa <cotellajoaquin@gmail.com>
2024-07-14 15:56:23 -0300

    Fixing hover hamburger button

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,37 +1,68 @@
 import React, { useState } from 'react';
-import "./Header.css"
 import {
     Box,
     Flex,
     IconButton,
-
+    Drawer,
+    DrawerBody,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+    useDisclosure
   } from '@chakra-ui/react'
 
-import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
+import { HamburgerIcon } from '@chakra-ui/icons'
+import "./Header.css";
 
 const Header = () => {
 
-    const [isHamburger, setIsHamburger] = useState(true);
-
-    const handleClick = () => {
-        setIsHamburger(!isHamburger);
-      };
+    // Hook para que se abra menu responsive
+    // Drawer Component
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const btnRef = React.useRef()
 
     return(
         <Box as="header">
-            <Flex alignItems='center' justifyContent='space-between'>
+            {/* Boton Hamburgesa */}
+            <Flex alignItems='center' justifyContent='space-around' width={ ['100%', '100%', '20%']}>
                 <IconButton
                 variant='outline'
-                className='btn-icon'
                 colorScheme='teal'
                 aria-label='Toggle menu'
-                icon={isHamburger ? <HamburgerIcon /> : <CloseIcon />}
+                icon={<HamburgerIcon />}
                 display={ {base: "inline", sm: "inline", md: "none"}}
-                onClick={handleClick}
+                onClick={onOpen}
+                _hover={{
+                    backgroundColor: 'black',
+                }}
                 />
-                <a class="logo">Jtella<span color='#04D9C3'>.</span></a>
+
+                <Drawer
+                    isOpen={isOpen}
+                    placement='rigth'
+                    onClose={onClose}
+                    finalFocusRef={btnRef}
+                    size='xs'
+                    >
+                    <DrawerOverlay />
+                    <DrawerContent>
+                    <DrawerCloseButton />
+                    <DrawerHeader>Portfolio Jtella</DrawerHeader>
+
+                    <DrawerBody >
+                        <p><a href="">Inicio</a></p>
+                        <p><a href="">Proyectos</a></p>
+                        <p><a href="">Habilidades</a></p>
+                        <p><a href="">Sobre mi</a></p>
+                        <p><a href="">Contactame</a></p>
+                    </DrawerBody>
+                    </DrawerContent>
+                </Drawer>
+
+                <a className="logo">Jtella<span color='#04D9C3'>.</span></a>
             </Flex>
-            <Flex alignItems='center' columnGap="2rem" wrap='wrap' display={ {base: 'none', sm: 'none', md:'flex'} }>
+            <Flex alignItems='center' justifyContent="space-between" columnGap="2rem" wrap='wrap' display={ {base: 'none', sm: 'none', md:'flex'}}>
                 <a href="#inicio" id="inicio" className='option'>Inicio</a>
                 <a href="#second-page" className='option'>Proyectos</a>
                 <a href="#third-page" className='option'>Habilidades</a>
