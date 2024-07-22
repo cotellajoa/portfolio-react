d54711dc3922f2e66f4bfe35b6d01629b7d987ef
jtella <cotellajoaquin@gmail.com>
2024-07-22 09:39:58 -0300

    Fixing responsive on projects page

--- src/pages/First/First.jsx
+++ src/pages/First/First.jsx
@@ -19,55 +19,55 @@ const First = () => {
             <Flex backgroundColor='blackAlpha' spacing='40px' padding='1rem' flexWrap='wrap' justifyContent='center' gap='1.5rem'>
                 <Card backgroundColor='blackAlpha' border='1px' borderColor='gray.400' width={{base: '260px', sm:'320px', md:'350px', lg:'420px'}}>
                     <CardHeader mb='0px'>
-                        <Heading size='md' color='white' textAlign='center'> Página para empresa</Heading>
+                        <Heading size={['xs','sm', 'md']} color='white' textAlign='center'> Página para empresa</Heading>
                     </CardHeader>
                     <CardBody mt='0'>
-                        <Image src="project1.png" alt="Proyecto 1" border='2px' borderColor='gray.400' height='245px'></Image>
+                        <Image src="project1.png" alt="Proyecto 1" border='2px' borderColor='gray.400' height={['180px','200px','245px']}></Image>
                     </CardBody>
                 </Card>
 
                 <Card backgroundColor='blackAlpha' border='1px' borderColor='gray.400' width={{base: '260px', sm:'320px', md:'350px', lg:'420px'}}>
                     <CardHeader mb='0px'>
-                        <Heading size='md' color='white' textAlign='center'> Clon de Trello</Heading>
+                        <Heading size={['xs','sm', 'md']} color='white' textAlign='center'> Clon de Trello</Heading>
                     </CardHeader>
                     <CardBody mt='0'>
-                        <Image src="project2.png" alt="Proyecto 1" border='2px' borderColor='gray.400' height='245px'></Image>
+                        <Image src="project2.png" alt="Proyecto 1" border='2px' borderColor='gray.400' height={['180px','200px','245px']}></Image>
                     </CardBody>
                 </Card>
 
                 <Card backgroundColor='blackAlpha' border='1px' borderColor='gray.400' width={{base: '260px', sm:'320px', md:'350px', lg:'420px'}}>
                     <CardHeader mb='0px'>
-                        <Heading size='md' color='white' textAlign='center'> Famoso juego "Uno"</Heading>
+                        <Heading size={['xs','sm', 'md']} color='white' textAlign='center'> Famoso juego "Uno"</Heading>
                     </CardHeader>
                     <CardBody mt='0'>
-                        <Image src="project3.png" alt="Proyecto 1" border='2px' borderColor='gray.400' height='245px'></Image>
+                        <Image src="project3.png" alt="Proyecto 1" border='2px' borderColor='gray.400' height={['180px','200px','245px']}></Image>
                     </CardBody>
                 </Card>
 
                 <Card backgroundColor='blackAlpha' border='1px' borderColor='gray.400' width={{base: '260px', sm:'320px', md:'350px', lg:'420px'}}>
                     <CardHeader mb='0px'>
-                        <Heading size='md' color='white' textAlign='center'> Página para E-commerce</Heading>
+                        <Heading size={['xs','sm', 'md']} color='white' textAlign='center'> Página para E-commerce</Heading>
                     </CardHeader>
                     <CardBody mt='0'>
-                        <Image src="project4.png" alt="Proyecto 1" border='2px' borderColor='gray.400' height='245px'></Image>
+                        <Image src="project4.png" alt="Proyecto 1" border='2px' borderColor='gray.400' height={['180px','200px','245px']}></Image>
                     </CardBody>
                 </Card>
 
                 <Card backgroundColor='blackAlpha' border='1px' borderColor='gray.400' width={{base: '260px', sm:'320px', md:'350px', lg:'420px'}}>
                     <CardHeader mb='0px'>
-                        <Heading size='md' color='white' textAlign='center'> Clon de Twitter</Heading>
+                        <Heading size={['xs','sm', 'md']} color='white' textAlign='center'> Clon de Twitter</Heading>
                     </CardHeader>
                     <CardBody mt='0'>
-                        <Image src="project5.png" alt="Proyecto 1" border='2px' borderColor='gray.400' height='245px'></Image>
+                        <Image src="project5.png" alt="Proyecto 1" border='2px' borderColor='gray.400' height={['180px','200px','245px']}></Image>
                     </CardBody>
                 </Card>
 
                 <Card backgroundColor='blackAlpha' border='1px' borderColor='gray.400' width={{base: '260px', sm:'320px', md:'350px', lg:'420px'}}>
                     <CardHeader mb='0px'>
-                        <Heading size='md' color='white' textAlign='center'> Content Management System</Heading>
+                        <Heading size={['xs','sm', 'md']} color='white' textAlign='center'> Content Management System</Heading>
                     </CardHeader>
                     <CardBody mt='0'>
-                        <Image src="project6.png" alt="Proyecto 1" border='2px' borderColor='gray.400' height='245px'></Image>
+                        <Image src="project6.png" alt="Proyecto 1" border='2px' borderColor='gray.400' height={['180px','200px','245px']}></Image>
                     </CardBody>
                 </Card>
             </Flex>
